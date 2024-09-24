11a7d5cd35173b82d7fba609cb74bdf8a76f2972
AlexMound <caophong3107aa@gmail.com>
2024-09-24 15:18:15 +0700

    Implement the JwtService class

---
+++ book-network/src/main/java/com/phong/book/security/JwtService.java
@@ -0,0 +1,12 @@
+package com.phong.book.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String generateToken(UserDetails userDetails) {
+
+    }
+}

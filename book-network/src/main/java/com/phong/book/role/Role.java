fdb43b6e06b15446f156fd7be198ef6eccbe4eaf
AlexMound <caophong3107aa@gmail.com>
2024-09-24 14:19:47 +0700

    Create the Token and repository

---
+++ book-network/src/main/java/com/phong/book/role/Role.java
@@ -0,0 +1,45 @@
+package com.phong.book.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.phong.book.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private List<User> users;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdDateTime;
+    @LastModifiedDate
+    @CreatedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDateTime;
+
+}

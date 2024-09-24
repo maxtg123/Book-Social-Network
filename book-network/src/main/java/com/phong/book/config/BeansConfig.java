f1b7bb4f46e7c873032ee015d3d0427412c9d626
AlexMound <caophong3107aa@gmail.com>
2024-09-24 14:46:00 +0700

    Implement the sevirity configuration class

---
+++ book-network/src/main/java/com/phong/book/config/BeansConfig.java
@@ -0,0 +1,9 @@
+package com.phong.book.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class BeansConfig {
+}
---
+++ book-network/src/main/java/com/phong/book/security/JwtFilter.java
@@ -0,0 +1,7 @@
+package com.phong.book.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtFilter {
+}
